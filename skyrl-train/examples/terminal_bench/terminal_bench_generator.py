9f14b00e00c2221cb245fcb181e0bb4b1c6a0c3c
Sumanth R Hegde <39546518+SumanthRH@users.noreply.github.com>
2025-10-06 15:12:04 -0700

    [Fix] Fix chat templating in Mini-SWE-Agent and Terminal-Bench examples (#404)
    Fixes chat templating in the Mini-swe-agent and the terminal bench
    examples.
    
    Previously, we were naively callling `.apply_chat_template` to encode
    response messages turn by turn - but this can append system messages for
    each turn depending on the model. (h/t to @CharlieFRuan )
    
    For example, Qwen 3 8B , the templating works fine, but then for Qwen
    2.5 1.5B instruct, the code adds a system prompt message while
    tokenizing every turn
    
    We use the fixed base approach similar to what we do in the
    SkyRLGymGenerator.
    
    ---------
    
    Signed-off-by: SumanthRH <sumanthrh99@gmail.com>

--- skyrl-train/examples/terminal_bench/terminal_bench_generator.py
+++ skyrl-train/examples/terminal_bench/terminal_bench_generator.py
@@ -4,7 +4,7 @@ from typing import List
 from loguru import logger
 from uuid import uuid4
 from skyrl_train.generators.base import GeneratorInterface, GeneratorInput, GeneratorOutput, TrajectoryID
-from skyrl_train.generators.utils import get_rollout_metrics
+from skyrl_train.generators.utils import get_rollout_metrics, encode_messages_subset
 from skyrl_train.inference_engines.inference_engine_client import InferenceEngineClient
 from skyrl_train.inference_engines.base import ConversationType
 from omegaconf import DictConfig
@@ -52,6 +52,9 @@ class TerminalBenchGenerator(GeneratorInterface):
         self.trials_dir = terminal_bench_cfg.trials_dir
         self.agent_name = terminal_bench_cfg.agent_name
         self.max_episodes = terminal_bench_cfg.max_episodes
+
+        if self.generator_cfg.chat_template.name_or_path is not None:
+            raise NotImplementedError("TerminalBenchGenerator doesn't support custom chat template")
 
     async def generate(self, input_batch: GeneratorInput) -> GeneratorOutput:
         tasks = []
@@ -201,7 +204,7 @@ class TerminalBenchGenerator(GeneratorInterface):
 
         for message in response_messages:
             # Apply chat template and tokenize each message
-            msg_encoding = self.tokenizer.apply_chat_template([message], add_generation_prompt=False, tokenize=True)
+            msg_encoding = encode_messages_subset([message], self.tokenizer)
 
             # Extend response_ids with the tokens
             response_ids.extend(msg_encoding)
